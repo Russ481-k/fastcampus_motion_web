5975db6317dbf4f7e4de2b761968edb0ab9b7940
Russ <yunsubin481@gmail.com>
2023-03-26 16:19:10 +0900

    setting

--- js/mask.js
+++ js/mask.js
@@ -110,10 +110,25 @@ btns.forEach((btn,index)=>{
     btn.addEventListener('click', e=>{
         for(let i=0; i<btns.length; i++) {
             btns[i].classList.remove('on');
-            panels[i].classList.remove('on');
+            
+            if(panels[i].classList.contains('on')){
+                panels[i].classList.add('mask');
+            }
         }
         btns[index].classList.add('on');
-        panels[index].classList.add('on');
+        panels[index].classList.add('lower');
+
+        setTimeout(()=>{
+            for(let i=0; i<panels.length; i++) {
+                if(panels[i].classList.contains('on')){
+                    panels[i].classList.remove('on');
+                    panels[i].classList.remove('mask');
+                }
+            }
+            panels[index].classList.remove('lower');
+            panels[index].classList.add('on');
+    
+        },1400);
     })
 })
 
